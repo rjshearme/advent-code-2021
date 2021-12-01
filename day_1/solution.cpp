a59d18b4106c57032f519762ca137a282c262994
rjshearme <rjshearme@gmail.com>
2021-12-01 18:06:52 +0000

    Day 1: completed both stars

--- day_1/solution.cpp
+++ day_1/solution.cpp
@@ -21,25 +21,43 @@ bool readFileContent(std::string fileName, std::vector<std::string> & fileLines)
     return true;
 }
 
-int countDepthIncreases(std::vector<std::string> & depths) {
-    int depth_increase_count;
+int countDepthIncreases(std::vector<int> & depths) {
+    int depth_increase_count = 0;
     for (int i=1; i < depths.size(); i++) {
         if (depths[i] > depths[i-1]) {
             depth_increase_count++;
         }
     }
-
     return depth_increase_count;
 }
 
 void solvePart1(std::vector<std::string> & fileLines) {
-    int depth_increase_count = countDepthIncreases(fileLines);
+    std::vector<int> depths;
+    for (std::string line : fileLines) {
+        depths.push_back(std::stoi(line));
+    }
+    int depth_increase_count = countDepthIncreases(depths);
     std::cout << "Depth increased " << depth_increase_count << " times" << std::endl;
+}
+
+void calculateDepthWindows(std::vector<int> & depthWindows, std::vector<int> & depths) {
+    for (int i=2; i < depths.size(); i++) {
+        int windowTotal = depths[i] + depths[i-1] + depths[i-2];
+        depthWindows.push_back(depths[i] + depths[i-1] + depths[i-2]);
+    }
+
 }
 
 
 void solvePart2(std::vector<std::string> & fileLines) {
-    std::cout << "Part 2 solved" << std::endl;
+    std::vector<int> depths;
+    for (std::string line : fileLines) {
+        depths.push_back(std::stoi(line));
+    }
+    std::vector<int> depthWindows;
+    calculateDepthWindows(depthWindows, depths);
+    int depth_increase_count = countDepthIncreases(depthWindows);
+    std::cout << "Depth increased over windows " << depth_increase_count << " times" << std::endl;
 }
 
 int readPartNumber(char* argv[]) {
